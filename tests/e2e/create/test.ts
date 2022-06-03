f916b8fd914ef41c3e535603a7f2c5b31fd354c8
cplepage <lepage.charlesphilippe@gmail.com>
2022-06-02 23:39:11 -0400

    pwa minimal setup

--- tests/e2e/create/test.ts
+++ tests/e2e/create/test.ts
@@ -2,12 +2,14 @@ import {describe} from "mocha";
 import {execSync} from "child_process";
 import fs from "fs";
 import path from "path";
-import Helper from "tests/e2e/Helper"
+import Helper from "fullstacked/tests/e2e/Helper"
 import {equal, ok} from "assert";
 
 describe("Create Test", function(){
     const webAppFile = path.resolve(__dirname, "index.tsx");
     const serverFile = path.resolve(__dirname, "server.ts");
+    const serviceWorkerFile = path.resolve(__dirname, "service-worker.ts");
+    const manifestFile = path.resolve(__dirname, "manifest.json");
     let test;
 
     it('Should create the default starter file', function(){
@@ -17,6 +19,15 @@ describe("Create Test", function(){
 
         ok(fs.existsSync(webAppFile));
         ok(fs.existsSync(serverFile));
+    });
+
+    it('Should add the pwa minimal requirements', function (){
+        const logMessage = execSync(`node ${path.resolve(__dirname, "../../../cli")} create --src=${__dirname} --silent --pwa --skip-test`).toString();
+        if(logMessage)
+            console.log(logMessage);
+
+        ok(fs.existsSync(serviceWorkerFile));
+        ok(fs.existsSync(manifestFile));
     });
 
     it('Should display the default starter app', async function (){
@@ -31,7 +42,7 @@ describe("Create Test", function(){
     after(async () => {
         await test.stop();
 
-        const files = [webAppFile, serverFile];
+        const files = [webAppFile, serverFile, serviceWorkerFile, manifestFile];
 
         files.forEach(file => {
             if(fs.existsSync(file))
