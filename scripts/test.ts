e7efac42dc91a94d33f8fb1d3a75119edeea438b
cplepage <lepage.charlesphilippe@gmail.com>
2022-06-03 10:12:35 -0400

    check if pull is needed before run

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,29 +1,23 @@
 import path from "path";
-import child_process from "child_process";
+import child_process, {execSync} from "child_process";
 
 //@ts-ignore
 process.env.FORCE_COLOR = true;
 
 export default function(config){
-    return new Promise<void>(resolve => {
-        const mochaConfigFile = path.resolve(__dirname, "../.mocharc.js");
+    const mochaConfigFile = path.resolve(__dirname, "../.mocharc.js");
 
-        // gather all test.ts files
-        // TODO: swap to **/*.test.ts maybe
-        const testFiles = path.resolve(process.cwd(), "**/test.ts");
+    // gather all test.ts files
+    // TODO: swap to **/*.test.ts maybe
+    const testFiles = path.resolve(process.cwd(), "**/test.ts");
 
-        let testCommand = `npx mocha "${testFiles}" --config ` + mochaConfigFile + " " +
-            (config.headless ? "--headless" : "") + " " +
-            (config.coverage ? "--coverage" : "");
+    let testCommand = `npx mocha "${testFiles}" --config ` + mochaConfigFile + " " +
+        (config.headless ? "--headless" : "") + " " +
+        (config.coverage ? "--coverage" : "");
 
-        // use nyc for coverage
-        if(config.coverage)
-            testCommand = "npx nyc --reporter text-summary --reporter html " + testCommand
+    // use nyc for coverage
+    if(config.coverage)
+        testCommand = "npx nyc --reporter text-summary --reporter html " + testCommand
 
-        const testProcess = child_process.exec(testCommand);
-        testProcess.stderr.pipe(process.stderr)
-        testProcess.stdout.pipe(process.stdout);
-
-        testProcess.on("exit", resolve);
-    });
+    execSync(testCommand, {stdio: "inherit"});
 }
