b7dfa5562cc3d640544b0e2b7a0fe075d14d2965
cplepage <lepage.charlesphilippe@gmail.com>
2022-06-27 16:11:01 -0400

    dont try to render in unexisting element

--- webapp/index.tsx
+++ webapp/index.tsx
@@ -4,6 +4,7 @@ import {createRoot} from "react-dom/client";
 
 export default function Webapp(app: ReactElement | string, rootElementID: string = "root") {
     const rootElement = document.getElementById(rootElementID);
+    if(!rootElement) return;
     const root = createRoot(rootElement);
     root.render(app);
 }
