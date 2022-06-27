63ee598d61795c550438946e84faf66f0aaf1a89
cplepage <lepage.charlesphilippe@gmail.com>
2022-06-27 14:48:08 -0400

    define root element id

--- webapp/index.tsx
+++ webapp/index.tsx
@@ -2,8 +2,8 @@ import "./GlobalReact";
 import {ReactElement} from "react";
 import {createRoot} from "react-dom/client";
 
-export default function Webapp(app: ReactElement | string) {
-    const rootElement = document.getElementById("root");
+export default function Webapp(app: ReactElement | string, rootElementID: string = "root") {
+    const rootElement = document.getElementById(rootElementID);
     const root = createRoot(rootElement);
     root.render(app);
 }
