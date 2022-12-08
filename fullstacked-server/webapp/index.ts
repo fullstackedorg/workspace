24315724a94489867fb6f7a08fc8a24836346910
cplepage <lepage.charlesphilippe@gmail.com>
2022-12-07 22:46:42 -0500

    moving to another project repo

--- fullstacked-server/webapp/index.ts
+++
@@ -1,6 +0,0 @@
-import main from "./main";
-
-const savedTheme = window.localStorage.getItem("theme");
-if(savedTheme === "dark") document.body.classList.add("theme-dark");
-
-main();
--- fullstacked-server/webapp/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import {createRoot} from "react-dom/client";
-import App from "./src/App";
-
-export default function (){
-    const root = createRoot(document.querySelector("fullstacked-root"));
-    root.render(<App />);
-}
