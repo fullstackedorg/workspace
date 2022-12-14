fd690222847f46feede714b500ed135657fad694
CPLepage <lepage.charlespilippe@gmail.com>
2022-12-14 08:59:19 -0500

    footer

--- gui/webapp/src/Header.tsx
+++ gui/webapp/src/Header.tsx
@@ -23,6 +23,17 @@ export default function (){
             <div className="navbar-nav flex-row order-md-last">
                 <div className="nav-item d-none d-md-flex me-3">
                     <div className="btn-list">
+                        <a href="https://docs.fullstacked.org" className="btn" target="_blank" rel="noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-book-2"
+                                 width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
+                                 fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z"></path>
+                                <path d="M19 16h-12a2 2 0 0 0 -2 2"></path>
+                                <path d="M9 8h6"></path>
+                            </svg>
+                            Docs
+                        </a>
                         <a href="https://github.com/cplepage/fullstacked" className="btn" target="_blank" rel="noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24"
                                  viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none"
--- gui/webapp/src/Steps.tsx
+++ gui/webapp/src/Steps.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {steps} from "./App";
 
-export default function ({stepIndex}){
+export default function ({stepIndex, goToStep}){
     return <div className={"card"}>
         <ul className="steps steps-counter my-4">
             {steps.map((step, index) =>
-                <li className={`step-item ${stepIndex === index ? "active" : ""}`}>{step.title}</li>)}
+                <li onClick={() => goToStep(index)} className={`cursor-pointer step-item ${stepIndex === index ? "active" : ""}`}>{step.title}</li>)}
         </ul>
     </div>
 }
