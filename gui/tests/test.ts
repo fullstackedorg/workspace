7b9a33b057cd74136d90cc4178d2ef23a7e90140
cplepage <lepage.charlesphilippe@gmail.com>
2022-12-18 12:03:24 -0500

    gui test

--- gui/tests/test.ts
+++ gui/tests/test.ts
@@ -1,14 +1,14 @@
 import { describe, it, before, after } from 'mocha';
-import HelperE2E from "../../tests/e2e/HelperE2E"
 import {ok} from "assert";
 import {dirname, resolve} from "path";
 import {fileURLToPath} from "url";
 import sleep from "../../utils/sleep.js";
+import TestE2E from "../../utils/testE2E.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 describe("GUI Test", function(){
-    let test = new HelperE2E(resolve(__dirname, ".."));
+    let test = new TestE2E(resolve(__dirname, ".."));
 
     before(async function (){
         await test.start();
