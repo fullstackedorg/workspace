2d0aba25de988e8c65559d51c487f7904ead95a3
CPLepage <lepage.charlesphilippe@gmail.com>
2022-12-23 22:50:25 -0500

    win path

--- build.ts
+++ build.ts
@@ -14,7 +14,7 @@ buildSync({
     sourcemap: true,
 });
 
-const buildRecursively = (await import(resolve(__dirname, "utils", "buildRecursively.js"))).default;
+const buildRecursively = (await import(resolve(__dirname, "utils", "buildRecursively.js").replace(/C:/, "").replace(/\\/, "/"))).default;
 
 const commands = glob.sync(resolve(__dirname, "commands", "**", "*.ts")).filter(file => !file.endsWith(".d.ts"));
 const types = glob.sync(resolve(__dirname, "types", "**", "*.ts"));
