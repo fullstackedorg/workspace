a9e64933093b17e70b99d18bbe56037e2069b849
cplepage <lepage.charlesphilippe@gmail.com>
2023-01-01 17:44:40 -0500

    changes and fixes

--- server.ts
+++ server.ts
@@ -6,72 +6,104 @@ import {fileURLToPath, pathToFileURL} from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-
 class ServerInstance {
     server: http.Server;
     watcher;
     port: number = 80;
     publicDir = path.resolve(__dirname, 'public');
-    logger: (req: IncomingMessage) => void = null;
-    reqListeners = [];
+    logger: {
+        in(req, res): void,
+        out(req, res): void
+    } = null;
+    listeners: {
+        title?: string,
+        handler(req: IncomingMessage, res: ServerResponse): void | Promise<void>,
+    }[] = [];
 
     constructor() {
         if(process.argv.includes("--development") || process.argv.includes("--gui")){
-            this.logger = (req: IncomingMessage) => {
-                console.log(req.method, req.url);
+            const activeRequests = new Map();
+            this.logger = {
+                in(req, res){
+                    activeRequests.set(res, Date.now());
+                    console.log(req.method, req.url);
+                },
+                out(req, res){
+                    const start = activeRequests.get(res) ?? 0;
+                    if(start) activeRequests.delete(res);
+                    console.log(req.method, req.url, `[${res.currentListener}]`, res.statusCode, (Date.now() - start) + "ms");
+                }
             }
         }
 
-        this.server = http.createServer(async (req, res) => {
-            if(this.logger) this.logger(req);
+        this.server = http.createServer(async (req, res: ServerResponse & {currentListener: string}) => {
+            if(this.logger?.in) this.logger.in(req, res);
 
-            for (const reqListener of this.reqListeners) {
+            for (const listener of this.listeners) {
                 // break if response has managed to send
-                if(res.headersSent) return;
+                if(res.headersSent) break;
 
-                const maybePromise = reqListener(req, res);
-                if(maybePromise instanceof Promise)
-                    await maybePromise;
+                res.currentListener = listener.title;
+
+                const maybePromise = listener.handler(req, res);
+                if(maybePromise instanceof Promise) {
+                    await new Promise<void>(async resolve => {
+                        let closed = false;
+                        res.on("close", () => {
+                            closed = true;
+                            resolve();
+                        });
+                        await maybePromise;
+                        if(!closed) resolve();
+                    });
+                }
             }
 
-            // response managed to send
-            if(res.headersSent) return;
+            // response did not manage to respond
+            if(!res.headersSent){
+                res.statusCode = 404;
 
-            // here we bottomed out of request listeners
-            res.writeHead(404);
-            res.end("Not Found");
+                res.currentListener = "Bottomed Out";
+                res.writeHead(res.statusCode);
+                res.end("Not Found");
+            }
+
+            if(this.logger?.out) this.logger.out(req, res);
         });
     }
 
     start(){
-        // default static file serving
-        this.reqListeners.unshift((req, res) => {
-            if(res.headersSent) return;
+        // default static file serving in front
+        this.listeners.unshift({
+            title: "Default Public File Serving",
+            handler: (req, res) => {
+                if (res.headersSent) return;
 
-            const url = new URL(this.publicDir + req.url, "http://localhost");
-            const filePath = url.pathname + (url.pathname.endsWith("/") ? "index.html" : "");
+                const url = new URL(this.publicDir + req.url, "http://localhost");
+                const filePath = url.pathname + (url.pathname.endsWith("/") ? "index.html" : "");
 
-            if(!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) return;
+                if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) return;
 
-            res.writeHead(200, {"content-type": mime.getType(filePath)});
-            res.end(fs.readFileSync(filePath));
+                res.writeHead(200, {"content-type": mime.getType(filePath)});
+                res.end(fs.readFileSync(filePath));
+            }
         });
 
         this.server.listen(this.port);
 
-        if(process.argv.includes("--development")){
-            import("./server/watcher").then(watcherModule => {
-                this.watcher = new watcherModule.default();
-                this.watcher.init(this.server);
-            });
-        }
+        if(!process.argv.includes("--development")) return;
+
+        import("./server/watcher").then(watcherModule => {
+            this.watcher = new watcherModule.default();
+            this.watcher.init(this.server);
+        });
     }
 
-    promisify(requestListener: RequestListener<typeof IncomingMessage, typeof ServerResponse>): {promisifiedListener(req, res): Promise<void>, resolver(req, res): void}{
+    promisify(requestListener: RequestListener<typeof IncomingMessage, typeof ServerResponse>): {handler(req, res): Promise<void>, resolver(req, res): void}{
         const requestMap = new Map<IncomingMessage, Function>();
 
         return {
-            promisifiedListener: (req, res) => new Promise<void>(resolve => {
+            handler: (req, res) => new Promise<void>(resolve => {
                 requestMap.set(req, resolve);
                 requestListener(req, res);
             }),
@@ -83,13 +115,6 @@ class ServerInstance {
                 requestMap.delete(req);
             }
         }
-    }
-
-    addListener(requestListener: RequestListener<typeof IncomingMessage, typeof ServerResponse>, prepend: boolean = false) {
-        if(prepend)
-            Server.reqListeners.unshift(requestListener);
-        else
-            Server.reqListeners.push(requestListener);
     }
 
     stop(){
--- tests/e2e/watch/server/template.ts
+++ tests/e2e/watch/server/template.ts
@@ -1,10 +1,13 @@
 import Server from "../../../../server.js";
 
 const bootTime = Date.now();
-Server.addListener((req, res) => {
-    if(req.url === "/bootTime"){
+Server.listeners.push({
+    handler(req, res) {
+        if (req.url !== "/bootTime") return;
+
         res.writeHead(200, {"content-type": "text/plain"});
         res.write(bootTime.toString());
         res.end();
     }
 });
+
