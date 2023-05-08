1e743a8d0335d8ac711ed59cfc74aa2686b01d84
cplepage <lepage.charlesphilippe@gmail.com>
2023-05-08 15:17:41 -0400

    wip smooth deployment

--- test.ts
+++ test.ts
@@ -21,7 +21,7 @@ execSync([
     `--prefix ${fileURLToPath(testDirectory)}`,
     "-y",
     "--",
-    `--tag ${packages.fullstackedPackage}`].join(" "), {cwd: testDirectory, stdio: "inherit"});
+    `--tag ${packages.fullstackedPackage} --ts`].join(" "), {cwd: testDirectory, stdio: "inherit"});
 
-// // testing local packages
+// testing local packages
 execSync(`npm i ${Object.keys(packages).map(pkg => packages[pkg]).join(" ")}`, {cwd: testDirectory, stdio: "inherit"});
