c03c38d8df40185894ff71c56560275a380c1066
cplepage <lepage.charlesphilippe@gmail.com>
2023-06-12 16:31:48 -0400

    added latency test for ide client

---
+++ packages/ide/client/latency/index.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from "react";
+import {client} from "../client";
+
+async function latencyTest(cb: (responsesTime: string[]) => void){
+
+}
+
+export default function () {
+    const [tests, setTests] = useState([]);
+    const [avg, setAvg] = useState(null);
+
+    useEffect(() => {
+        if(tests.length === 10){
+            setAvg(tests.reduce((tot, time) => tot + time, 0) / tests.length)
+            return;
+        }
+
+        const start = performance.now();
+        client.get().ping().then(response => {
+            const result = performance.now() - start;
+            setTests([...tests, result]);
+        })
+    }, [tests]);
+
+    return <div style={{padding: 5, color: "white"}}>
+        <div>Page load: {performance.timing.loadEventEnd - performance.timing.navigationStart}ms</div>
+        <hr />
+        {tests.map(test => <div>
+            Response time: {test.toFixed(2)}ms
+        </div>)}
+        <hr />
+        {avg && <div>
+            Average ping time: {avg.toFixed(2)}ms
+        </div>}
+    </div>
+}
