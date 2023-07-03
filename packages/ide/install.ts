c1b76eab93d81e9bd2bec990722d871d2c7ab583
cplepage <lepage.charlesphilippe@gmail.com>
2023-07-03 09:54:34 -0400

    a change in focus

--- packages/ide/install.ts
+++
@@ -1,4 +0,0 @@
-import * as packages from "../../pack";
-import {execSync} from "child_process";
-
-execSync(`npm i -g ${Object.keys(packages).map(pkg => packages[pkg]).join(" ")}`, {stdio: "inherit"});
--- utils/randStr.ts
+++
@@ -1,5 +0,0 @@
-import {randomBytes} from "crypto";
-
-export default function (){
-    return randomBytes(6).toString('hex');
-}
