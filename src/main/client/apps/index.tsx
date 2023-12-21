c07df14bd61fc019f1d031d8aca019b435229b4d
cplepage <lepage.charlesphilippe@gmail.com>
2023-12-21 00:24:42 -0500

    files

---
+++ src/main/client/apps/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { client } from "../client"
+import { SyncWS, centeredPopupFeatures } from "../sync/Indicator";
+import { Workspace } from "../workspace";
+
+function AppLauncher(props: {app, didSpawn}){
+    const [failedOpen, setFailedOpen] = useState(false);
+    const [url, setUrl] = useState(null);
+
+    const openApp = (url: string) => {
+        const win = window.open(url, "", centeredPopupFeatures());
+        setFailedOpen(!win);
+        if(win)
+            props.didSpawn()
+    }
+
+    useEffect(() => {
+        client.post().runApp(props.app.entrypoint)
+            .then(url => {
+                setUrl(url);
+                openApp(url);
+            })
+    }, [])
+
+    return <div className={"prepare-fs-remote"}>
+        {url 
+            ? failedOpen 
+                ? <button onClick={() => openApp(url)}>Open {props.app.title}</button>
+                : <div>{props.app.title} Running</div>
+            : <div>Loading {props.app.title}</div>}
+    </div>
+}
+
+function loadLocalApps(){
+    client.get().listApps().then(apps => {
+        apps.forEach(app => {
+            if(Workspace.instance.apps.find(({title}) => title === app.title))
+                return;
+
+            Workspace.addApp({
+                title: app.title,
+                icon: app.icon,
+                element: (wApp) => <AppLauncher didSpawn={Workspace.instance.removeWindow(wApp)} app={app} />
+            })
+        })
+    })
+}
+
+SyncWS.subscribers.add(status => {
+    if(SyncWS.isSynced(status))
+        loadLocalApps();
+});
